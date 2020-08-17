c92b57330e57d137eb6174b9ac6f597c7f977caa
Samridhi <samridhikots@gmail.com>
2020-08-17 18:36:53 +0530

    Final app

---
+++ app/src/main/java/com/samridhi/musicPlanet/models/Artist.java
@@ -0,0 +1,37 @@
+package com.samridhi.musicPlanet.models;
+
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Artist {
+
+    public final List<Album> albums;
+
+    public Artist() {
+        this.albums = new ArrayList<>();
+    }
+
+    public int getId() {
+        return getFirstAlbum().getArtistId();
+    }
+
+    public String getName() {
+        return getFirstAlbum().getArtistName();
+    }
+
+    @NonNull
+    private Album getFirstAlbum() {
+        return albums.isEmpty() ? new Album() : albums.get(0);
+    }
+
+    public int getSongCount() {
+        int songCount = 0;
+        for (Album album : albums) {
+            songCount += album.getSongCount();
+        }
+        return songCount;
+    }
+}
